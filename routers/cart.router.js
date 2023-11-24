f03b5b06b3bfd2dff609fb418138beeefe9a2f93
Nguyen Van Loc <loccosi3005@gmail.com>
2023-11-24 17:05:13 +0700

    update cart

--- routers/cart.router.js
+++ routers/cart.router.js
@@ -9,6 +9,6 @@ router.get(
   cartController.getCartByAccount
 );
 router.post("/cart", cartController.createCart);
-router.put("/cart/:id", authMidleWare.apiAuth, cartController.updateCart);
-router.delete("/cart/:id", authMidleWare.apiAuth, cartController.deleteCart);
+router.put("/cart/:id", cartController.updateCart);
+router.delete("/cart/:id", cartController.deleteCart);
 module.exports = router;
